a87fc9b9c24fa56fc4edcf799272260d7ff25cd2
Hyunjun Kim <hyunjun2372@gmail.com>
2024-07-19 14:01:29 +0000

    Add MariaDB and WordPress Dockerfiles and Startup scripts

---
+++ mariadb/Startup.sh
@@ -0,0 +1,30 @@
+
+# Log when error occurs
+set -x
+set +e
+
+
+# Open external access to MariaDB
+if ! [ -f //etc/mysql/mariadb.cnf ]; then
+  echo "[Assertion Failed] MariaDB configuration file is not found."
+fi
+sed -i 's/bind-address/\# bind-address/g'  /etc/mysql/mariadb.cnf
+
+# Start MariaDB
+service mysql start
+while ! mysqladmin ping; do
+    sleep 1
+done
+
+# DB Query
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -uroot -e "USE $DB_NAME;"
+mysql -uroot -e "CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_USER_PW';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PW';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+
+#mysqladmin -uroot -p shutdown
+while ! mysqladmin ping; do
+    sleep 1
+done
+service mariadb stop && mysqld;
