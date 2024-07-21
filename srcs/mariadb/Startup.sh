05b86e20b11bd1cd024a8892f0ab26a9d8cad6e0
hyunjunk <hyunjun2372@gmail.com>
2024-07-21 21:35:56 +0900

    fix

--- srcs/mariadb/Startup.sh
+++ srcs/mariadb/Startup.sh
@@ -1,28 +1,23 @@
 #!/bin/sh
 
 # Open external access to MariaDB
-service mysql start
 
-if [ ! -e "/run/mysqld/mysqld.sock" ]; then
-  mysql_install_db
-  mysqld_safe 
+mysql_install_db
 
-  sed -i 's/bind-address/\# bind-address/g'  /etc/mysql/mariadb.cnf
+/etc/init.d/mysql start
 
-  # Start MariaDB
-  while ! mysqladmin -uroot ping; do
-      sleep 1
-  done
 
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+  # sed -i 's/bind-address/\# bind-address/g'  /etc/mysql/mariadb.cnf
+  
   # DB Query
   mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
   mysql -uroot -e "USE $DB_NAME;"
   mysql -uroot -e "CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_USER_PW';"
   mysql -uroot -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PW';"
   mysql -uroot -e "FLUSH PRIVILEGES;"
-
-  mysqladmin --user=root shutdown
-
 fi
+
+/etc/init.d/mysql stop
 
 mysqld_safe --datadir=/var/lib/mysql/data --user=root --port=3306 --default-file=/tmp
