e64e8bae5f21c8cc423c32c2c9ade2fc5ed17a72
hyunjunk <hyunjun2372@gmail.com>
2024-07-21 19:59:57 +0900

    fix simpley

--- srcs/mariadb/Startup.sh
+++ srcs/mariadb/Startup.sh
@@ -3,14 +3,12 @@
 # Open external access to MariaDB
 if [ ! -e "/run/mysqld/mysqld.sock" ]; then
   mysql_install_db
-  /usr/share/mariadb/mysql.server start
-  /etc/init.d/mysql start
-  mysqladmin -u root -p status
+  mysqld_safe 
 
   sed -i 's/bind-address/\# bind-address/g'  /etc/mysql/mariadb.cnf
 
   # Start MariaDB
-  while ! mysqladmin --user=root ping; do
+  while ! mysqladmin -uroot ping; do
       sleep 1
   done
 
@@ -20,11 +18,6 @@ if [ ! -e "/run/mysqld/mysqld.sock" ]; then
   mysql -uroot -e "CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_USER_PW';"
   mysql -uroot -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PW';"
   mysql -uroot -e "FLUSH PRIVILEGES;"
-
-  #mysqladmin -uroot -p shutdown
-  while ! mysqladmin --user=root ping; do
-      sleep 1
-  done
 
   mysqladmin --user=root shutdown
 
