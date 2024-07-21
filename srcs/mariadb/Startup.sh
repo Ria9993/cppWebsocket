601c91c22d15f2b4b6daea726c5df2f23e317557
hyunjunk <hyunjun2372@gmail.com>
2024-07-21 21:47:06 +0900

    fix

--- srcs/mariadb/Startup.sh
+++ srcs/mariadb/Startup.sh
@@ -4,20 +4,21 @@
 
 mysql_install_db
 
-/etc/init.d/mysql start
+service mysql start
 
 
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
   # sed -i 's/bind-address/\# bind-address/g'  /etc/mysql/mariadb.cnf
-  
+
   # DB Query
   mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
   mysql -uroot -e "USE $DB_NAME;"
-  mysql -uroot -e "CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_USER_PW';"
-  mysql -uroot -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PW';"
+  mysql -uroot -e "CREATE USER IF NOT EXISTS '{$DB_USER}'@'%' IDENTIFIED BY '$DB_USER_PW';"
+  mysql -uroot -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '{$DB_USER}'@'%' IDENTIFIED BY '$DB_USER_PW';"
+  mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ADMIN_PW';"
   mysql -uroot -e "FLUSH PRIVILEGES;"
 fi
 
-/etc/init.d/mysql stop
+service mysql stop
 
-mysqld_safe --datadir=/var/lib/mysql/data --user=root --port=3306 --default-file=/tmp
+mysqld
