1ad9623eab1cae2854093bc367ffa552c8375f2a
hyunjunk <hyunjun2372@gmail.com>
2024-07-21 22:02:09 +0900

    fix

--- srcs/mariadb/Startup.sh
+++ srcs/mariadb/Startup.sh
@@ -6,17 +6,16 @@ mysql_install_db
 
 service mysql start
 
-
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
+if [ ! -d "/var/lib/mysql/data/wordpress" ]; then
   # sed -i 's/bind-address/\# bind-address/g'  /etc/mysql/mariadb.cnf
 
   # DB Query
-  mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-  mysql -uroot -e "USE $DB_NAME;"
-  mysql -uroot -e "CREATE USER IF NOT EXISTS '{$DB_USER}'@'%' IDENTIFIED BY '$DB_USER_PW';"
-  mysql -uroot -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '{$DB_USER}'@'%' IDENTIFIED BY '$DB_USER_PW';"
-  mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ADMIN_PW';"
-  mysql -uroot -e "FLUSH PRIVILEGES;"
+  mysql -uroot -e "\
+        CREATE DATABASE IF NOT EXISTS $DB_NAME; \
+        CREATE USER IF NOT EXISTS '{$DB_USER}'@'%' IDENTIFIED BY '$DB_USER_PW'; \
+        GRANT ALL ON $DB_NAME.* TO '{$DB_USER}'@'%'; \
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ADMIN_PW'; \
+        FLUSH PRIVILEGES;"
 fi
 
 service mysql stop
