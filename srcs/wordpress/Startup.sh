573d3f653319f4e3d53f6ef167abf5f73ebfd380
Hyunjun Kim <hyunjun2372@gmail.com>
2024-07-23 00:20:49 +0900

    a

--- srcs/wordpress/Startup.sh
+++ srcs/wordpress/Startup.sh
@@ -12,13 +12,15 @@ echo "env[DB_PASSWORD] = $DB_USER_PW" >> ${PHP_FPM_POOL_CONF}
 # @see https://dev.mysql.com/doc/refman/8.0/en/mysqladmin.html
 POLLING_INTERVAL=1
 POLLING_COUNTER=0
-while ! mysqladmin ping --silent -hmariadb -u$DB_USER -p$DB_USER_PW; do
+
+until mysql --host=$DB_HOST --user=$DB_USER --password=$DB_USER_PWD -e '\c'; do
+  echo >&2 "mariadb is unavailable - sleeping"
   POLLING_COUNTER=$((POLLING_COUNTER + 1))
   if [ "$POLLING_COUNTER" -eq "$WAIT_TIMEOUT" ]; then
     echo "MariaDB is not ready. (WaitTime=${POLLING_COUNTER})"
     exit 1
   fi
-  sleep ${POLLING_INTERVAL}
+  sleep sleep ${POLLING_INTERVAL}
 done
 echo "Checked MariaDB alive."
 
@@ -31,34 +33,13 @@ sed -i 's|;cgi.fix_pathinfo=1|cgi.fix_pathinfo=0|g' ${PHP_FPM_POOL_CONF}
 sed -i 's|;clear_env|clear_env|g' ${PHP_FPM_POOL_CONF}
 
 
-# wp-cli. Set up the WordPress
-if ! [ -e "/var/www/wordpress/wp-config.php" ]; then 
+rm -f /var/www/html/wp-config.php
+chown -R www-data:www-data /var/www/html
+/wp-cli.phar config create --allow-root --dbname="$WP_DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_USER_PWD" --dbhost="$DB_HOST" --dbcharset=utf8mb4 --path=/var/www/html
 
-  # @see https://make.wordpress.org/cli/handbook/guides/installing/#recommended-installation
-  curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-  chmod +x wp-cli.phar
-  mv -f wp-cli.phar /usr/local/bin/wp
+wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --path=/var/www/html --allow-root
 
-  wp core download --allow-root
-  wp config create --allow-root \
-    --dbname=$DB_DATABASE \
-    --dbuser=$DB_USER \
-    --dbpass=$DB_USER_PW \
-    --dbhost=mariadb:$DB_PORT \
-    --skip-check
-  wp core install --allow-root \
-    --url=$DOMAIN_NAME \
-    --title=$WP_TITLE \
-    --admin_user=$WP_USER \
-    --admin_password=$WP_USER_PW \
-    --admin_email=$WP_USER_EMAIL
-  wp user create --allow-root \
-    $WP_USER \
-    $WP_USER_EMAIL \
-    --user_pass=$WP_USER_PW
-else
-  echo "[ASSERTION FAILED] wp-cli is already installed."
-fi
+wp user create "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PWD" --allow-root --path=/var/www/html
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 	exit 1
